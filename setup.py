a546c9a7d50c0a13d3a101741c02549cc7c1f76f
Marat Kazanov <mkazanov@gmail.com>
2023-11-19 01:01:25 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+setup(
+ name='rnasselem',
+ version='1.0',
+ description='a Python package for descriptive analysis of RNA secondary structure elements in viral genomes',
+ author='Fedor Kazanov, Evgenii Matveev, Gennady Ponomarev, Dmitry Ivankov, Marat Kazanov',
+ author_email='mkazanov@gmail.com',
+ url='https://github.com/KazanovLab/RNAsselem',
+ packages=[]
+)
+ 
+
+
